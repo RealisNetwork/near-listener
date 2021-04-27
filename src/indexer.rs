68ab3380fe4d858f091df2d8c7a24de130ccc615
jasperdg <jasperdegooijer@gmail.com>
2021-04-27 16:59:54 +0200

    updated nearcore and implemented indexer framework changes for this nearcore version

--- src/indexer.rs
+++ src/indexer.rs
@@ -6,14 +6,16 @@ pub async fn handle_blocks_message(capacitor_ins: Arc<Mutex<Capacitor>>, mut str
     while let Some(streamer_message) = stream.recv().await {
         println!("⛏ Block height {:?}", streamer_message.block.header.height);
         let capacitor_unwrapped = capacitor_ins.lock().unwrap();
-        for chunk in streamer_message.chunks {
-            for tx_res in chunk.receipt_execution_outcomes {
+        
+        for shard in streamer_message.shards {
+            for tx_res in shard.receipt_execution_outcomes {
                 if !capacitor_unwrapped.is_valid_receipt(&tx_res.execution_outcome) {
                     continue;
                 }
     
                 capacitor_unwrapped.process_outcome(tx_res.execution_outcome.outcome).await;
             }
+
         }
     }
 }
